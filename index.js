5c7e565744615e1b5eaeee1c90cb5a5ad3515365
Kapil Baser <94954247+kapilBaser@users.noreply.github.com>
2024-01-24 00:26:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,4 +23,76 @@ function generateBars(){
 generateArray();
 generateBars();
 
+async function selectionSort(delay = 300) {
 
+    //  return all div array whose class=bar
+    let bars = document.querySelectorAll(".bar");
+
+
+    for (var i = 0; i < bars.length - 1; i++) {
+
+        await new Promise((resolve) =>
+            setTimeout(() => {
+                resolve();
+            }, speed)
+
+        );
+
+        bars[i].style.backgroundColor = "rgb(253, 93, 0)";
+
+        for (var j = i + 1; j < bars.length; j++) {
+
+            bars[j].style.backgroundColor = 'rgb(253, 93, 0)';
+
+            //await function to pause for a while
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, speed)
+
+            );
+            if (arr[i] > arr[j]) {
+
+                bars[j].style.backgroundColor = "red";
+
+                //await function to pause for a while
+                await new Promise((resolve) =>
+                    setTimeout(() => {
+                        resolve();
+                    }, speed)
+
+                );
+
+                var temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                bars[i].style.height = arr[i] + 'px';
+                bars[j].style.height = arr[j] + 'px';
+            }
+
+            if (i == bars.length - 2) {
+
+                bars[i].style.transitionProperty = "background-color";
+                bars[i].style.transitionDuration = '1s';
+                bars[j].style.backgroundColor = "green";
+            } 
+            else{
+                bars[j].style.backgroundColor = "rgb(43, 42, 42)";
+            }
+            //await function to pause for a while
+            await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, speed)
+
+            );
+        } // for loop end
+
+        bars[i].style.transitionProperty = "background-color";
+        bars[i].style.transitionDuration = '1s';
+        bars[i].style.backgroundColor = "green ";
+
+    } // for loop end
+
+   
+}
