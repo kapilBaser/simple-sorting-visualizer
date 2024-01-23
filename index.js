c254f887b8572e00112906f3794678fd16469056
Kapil Baser <94954247+kapilBaser@users.noreply.github.com>
2024-01-24 00:24:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ function generateArray(){
 }
 
 function generateBars(){
-    
+    for (var i = 0; i < 9; i++) {
+        let bar = document.createElement('div');
+        console.log(arr[i])
+        bar.style.display = 'inline-block';
+        bar.classList.add('bar');
+       
+        bar.style.height = arr[i] + 'px';
+
+        container.appendChild(bar);
+    } 
 }
+
+generateArray();
+generateBars();
+
 
