1b6e0cec17dffe60005358e30073930f8d431e95
Kapil Baser <94954247+kapilBaser@users.noreply.github.com>
2024-01-23 23:55:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const container = document.getElementById('container')
 const button = document.getElementsByTagName('button')[0]
+
+let arr = []
+function generateArray(){
+    for(var i = 0; i < 9; i++)
+        arr[i] = Math.floor(Math.random()*300)+10;
+}
